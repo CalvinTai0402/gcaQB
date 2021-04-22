4f2a92425c5d45fa1f0d6394c5ee7c1184d5895a
CalvinTai0402 <jfctai@rams.colostate.edu>
2021-04-22 04:19:50 +0000

    added admin panel

---
+++ resources/js/components/App.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import ReactDOM from 'react-dom'
+import { Admin, Resource } from 'react-admin';
+import { UserList } from './users';
+import { PostList, PostEdit, PostCreate } from './posts';
+import { CustomerCreate } from './customers';
+import jsonServerProvider from 'ra-data-json-server';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import CustomerIcon from '@material-ui/icons/PersonAddSharp';
+import Dashboard from './Dashboard';
+import authProvider from './authProvider';
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const App = () => (
+    <div>
+        <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+            <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+            <Resource name="users" list={UserList} icon={UserIcon} />
+            <Resource name="customers" create={CustomerCreate} icon={CustomerIcon} />
+        </Admin>
+    </div>
+
+);
+
+export default App;
+
+if (document.getElementById('admin')) {
+    ReactDOM.render(<App />, document.getElementById('admin'));
+}
---
+++ resources/js/components/authProvider.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+export default {
+    // called when the user attempts to log in
+    login: ({ username }) => {
+        localStorage.setItem('username', username);
+        // accept all username/password combinations
+        return Promise.resolve();
+    },
+    // called when the user clicks on the logout button
+    logout: () => {
+        axios.post('http://127.0.0.1:8000/logout').then(
+            window.location.href = '/'
+        ).catch(error => {
+            console.log("ERROR:: ", error.response.data);
+        });
+    },
+    // called when the API returns an error
+    checkError: ({ status }) => {
+        if (status === 401 || status === 403) {
+            localStorage.removeItem('username');
+            return Promise.reject();
+        }
+        return Promise.resolve();
+    },
+    // called when the user navigates to a new location, to check for authentication
+    checkAuth: () => {
+        return localStorage.getItem('username')
+            ? Promise.resolve()
+            : Promise.reject();
+    },
+    // called when the user navigates to a new location, to check for permissions / roles
+    getPermissions: () => Promise.resolve(),
+};
