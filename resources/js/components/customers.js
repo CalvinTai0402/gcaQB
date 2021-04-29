753ba190bdc6f6bbc3d2eacb9d0d4914437985c9
CalvinTai0402 <jfctai@rams.colostate.edu>
2021-04-29 21:43:15 +0000

    added while loop debounce

--- resources/js/components/customers.js
+++ resources/js/components/customers.js
@@ -23,31 +23,43 @@ function sleep(ms) {
 let timerIdCreate;
 let errorsCreate = {};
 const validateCustomerCreation = async (values) => {
+    let returnedErrorsCreate;
+    errorsCreate = { breakLoop: 0 };
     if (timerIdCreate) {
         clearTimeout(timerIdCreate);
     }
     timerIdCreate = setTimeout(async () => {
         errorsCreate = await validateFields(values, "create")
     }, 2000)
-    await sleep(4000);
-    return errorsCreate;
+    do {
+        await sleep(100);
+    } while (Object.keys(errorsCreate).length === 0 || (Object.keys(errorsCreate).length === 1 && errorsCreate.breakLoop === 0));
+    returnedErrorsCreate = JSON.parse(JSON.stringify(errorsCreate));
+    delete returnedErrorsCreate.breakLoop;
+    return returnedErrorsCreate;
 };
 
 let timerIdEdit;
 let errorsEdit = {};
 const validateCustomerEdit = async (values) => {
+    let returnedErrorsEdit;
+    errorsEdit = { breakLoop: 0 };
     if (timerIdEdit) {
         clearTimeout(timerIdEdit);
     }
     timerIdEdit = setTimeout(async () => {
         errorsEdit = await validateFields(values, "edit")
     }, 2000)
-    await sleep(4000);
-    return errorsEdit;
+    do {
+        await sleep(100);
+    } while (Object.keys(errorsEdit).length === 0 || (Object.keys(errorsEdit).length === 1 && errorsEdit.breakLoop === 0));
+    returnedErrorsEdit = JSON.parse(JSON.stringify(errorsEdit));
+    delete returnedErrorsEdit.breakLoop;
+    return returnedErrorsEdit;
 };
 
 const validateFields = (values, editOrCreate) => {
-    const errors = {};
+    const errors = { breakLoop: 0 }; // 0 means to not break loop, 1 is to break loop
     var promise = new Promise(function (resolve, reject) {
         setTimeout(async () => {
             if (!values.title) {
@@ -76,6 +88,7 @@ const validateFields = (values, editOrCreate) => {
             if (!isEmailUnique) {
                 errors.email = 'Email already used';
             }
+            errors.breakLoop = 1
             resolve(errors)
         });
     });
